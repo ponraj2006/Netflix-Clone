1c527560e9aed1f01ea920709682de011bceeff0
ponraj_k <ponrajk2006@gmail.com>
2026-01-01 09:12:07 +0530

    Improve Home and add title card

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React,{useEffect, useRef} from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
+
+
+const cardsRef = useRef();
+
+useEffect(()=>{
+  cardsRef.current.addEventListenner('wheel',handleWheel)
+},[])
+
 const TitleCards = () => {
   return (
-    <div className='titlecards'></div>
+    <div className='titlecards'>
+      <h2>Popular on Netflix</h2>
+      <div class="card-list" ref={cardsRef}>
+        {
+          cards_data.map((card,index)=>{
+            return <div class="card" key={index}>
+              <img src={card.image} alt=""/>
+              <p>{card.name}</p>
+            </div>
+          })
+        }
+      </div>
+    </div>
   )
 }
 
