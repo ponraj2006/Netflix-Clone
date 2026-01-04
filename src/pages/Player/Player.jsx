0f3e26c0eac06d0f0857537709ebc830c645b62b
ponraj_k <ponrajk2006@gmail.com>
2026-01-04 21:05:24 +0530

    To create Player and Assiging data to tittle card

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import './player.css'
+import React from "react";
+import "./player.css";
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
 const Player = () => {
   return (
-    <div className='player'></div>
-  )
-}
+    <div className="player">
+      <img src={back_arrow_icon} alt="" />
+      <iframe
+        width="90%"
+        height="90%"
+        src='https://www.youtube.com/embed/EdxCI2KozWxcyWI5'
+        title="trailer"
+        frameBorder='0'
+        allowFullScreen
+      ></iframe>
+      <div class="player-info">
+        <p>Published Date</p>
+        <p>Name</p>
+        <p>Type</p>
+      </div>
+    </div>
+  );
+};
 
-export default Player
+export default Player;
